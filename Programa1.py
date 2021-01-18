23b0cd76f18f68c832fc77ed6c05cc495c32d5ee
esquiveldag <77644838+esquiveldag@users.noreply.github.com>
2021-01-18 16:33:14 -0600

    Add files via upload
    Uploading a pythons script

---
+++ Programa1.py
@@ -0,0 +1,36 @@
+print("Esto es  una suma")
+numero_uno = 4
+numero_dos = 2
+resultado = numero_uno + numero_dos
+print(resultado)
+
+mensaje = "Hola"
+mensaje+= " "
+mensaje+= "Daniela"
+print(mensaje)
+ 
+print("Concatenación")
+mensaje = "Hola"
+espacio = " "
+nombre = "Daniela"
+print(mensaje + espacio + nombre)
+
+numero_uno = 4
+numero_dos = 2
+resultado = numero_uno + numero_dos
+resultado = str(resultado)
+print("El resultado de la suma es:" + resultado)
+
+print("Busqueda:")
+mensaje = "Hola Daniela"
+buscar_subcadena = mensaje.find("Daniela")
+print(buscar_subcadena)
+
+mensaje = "Hola Daniela"
+extraer_subcadena = mensaje[0:9]
+print(extraer_subcadena)
+
+mensaje_uno = "Hola"
+mensaje_dos= "hola"
+print(mensaje_uno == mensaje_dos)
+
